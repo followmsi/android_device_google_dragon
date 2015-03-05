e6ea80231dc6e1dc526be19b06c94f78ae3bdb0c
Lorenzo Colitti <lorenzo@google.com>
2015-03-05 16:05:23 +0900

    Remove explicit Ethernet support, the device supports USB OTG.
    Change-Id: I71ed8673c9c360e0b7d98be50e7a38d7b4295b39
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- device.mk
+++ device.mk
@@ -57,8 +57,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
-    frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
-    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio_policy.conf:system/etc/audio_policy.conf \
