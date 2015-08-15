e2e0596b1fac11680bc51d7abaad30d134e98b42
Katie Roberts-Hoffman <katierh@google.com>
2015-08-14 18:17:46 -0700

    dragon: update hwui parameters for screen resolution
    BUG=23227496
    
    Change-Id: I6c9580777c8a620b2d9d73e9f412d5fe003afa3d

--- device.mk
+++ device.mk
@@ -107,7 +107,19 @@ PRODUCT_PACKAGES += \
 
 #TODO(dgreid) is this right?
 PRODUCT_PROPERTY_OVERRIDES := \
-    wifi.interface=wlan0
+    wifi.interface=wlan0 \
+    ro.hwui.texture_cache_size=86 \
+    ro.hwui.layer_cache_size=56 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.path_cache_size=40 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=1024 \
+    ro.hwui.disable_scissor_opt=true
 
 # setup dalvik vm configs.
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
