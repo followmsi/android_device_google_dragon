258008848a494e68a66e9a638582b385687394a1
David Riley <davidriley@google.com>
2015-08-27 17:16:04 -0700

    dragon: change TARGET_BOARD_PLATFORM to tegra210_dragon
    Change-Id: I23466beb6799dac15d68c9b59078281a1876bd5d
    Signed-off-by: David Riley <davidriley@google.com>

--- audio/Android.mk
+++ audio/Android.mk
@@ -1,4 +1,4 @@
-ifneq ($(filter tegra132_dragon,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter tegra210_dragon,$(TARGET_BOARD_PLATFORM)),)
 ifneq ($(filter dragon,$(TARGET_DEVICE)),)
 
 MY_LOCAL_PATH := $(call my-dir)
