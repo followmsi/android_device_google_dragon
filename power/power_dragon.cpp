d10498c246c88ca14d550cc843d42a30e2b65536
Xia Yang <xiay@nvidia.com>
2015-11-12 16:14:48 -0800

    dragon: power: change GPU boost pstate to 0x0b
    BUG=24682588
    TEST=run Chrome overflow menu render jank test and observe systrace
    
    Change-Id: I9de770ca417044d95910e9d3f403be14a4a4006a

--- power/power_dragon.cpp
+++ power/power_dragon.cpp
@@ -47,7 +47,7 @@
 #define LOW_POWER_MAX_FREQ "1020000"
 #define NORMAL_MAX_FREQ "1912500"
 #define GPU_BOOST_PATH "/sys/devices/57000000.gpu/pstate"
-#define GPU_BOOST_ENTER_CMD "03,0C"    // boost GPU to work at least on 03 - 230MHz
+#define GPU_BOOST_ENTER_CMD "0b,0C"    // boost GPU to work at least on 0b - 844MHz
 #define GPU_BOOST_DURATION_MS 40
 #define GPU_BOOST_EXIT_CMD "auto"
 #define GPU_FREQ_CONSTRAINT "852000 852000 -1 2000"
