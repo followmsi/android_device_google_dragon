fe05a1c89f7169b8f5be3228357324f29a772846
Ajay Dudani <adudani@google.com>
2017-01-17 13:03:20 -0800

    Add gatekeeper HAL for pass-through mode
    Bug: 33686345
    Test: Verified ryu upgrade from nyc to master with PIN enabled
    Change-Id: I00849dbbbe6385aaedb4a22c0fbcdb9100309103
    Signed-off-by: Ajay Dudani <adudani@google.com>

--- device.mk
+++ device.mk
@@ -165,6 +165,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl
 
+# Gatekeeper HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl
+
 ifeq ($(ENABLE_TREBLE), true)
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-service
