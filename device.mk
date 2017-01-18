bd2773c03d12209dac55fbd8b7ecb4582cf8c1ec
Roshan Pius <rpius@google.com>
2017-01-18 14:13:12 -0800

    ryu: Add wifi HAL service to device.mk
    Bug: 33460969
    Test: Compiles.
    Change-Id: I496cf01efd323e78e1d96888f733935cefa8c002

--- device.mk
+++ device.mk
@@ -55,6 +55,7 @@ PRODUCT_COPY_FILES := \
     $(LOCAL_PATH)/com.nvidia.nvsi.xml:system/etc/permissions/com.nvidia.nvsi.xml
 
 PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service \
     libwpa_client \
     hostapd \
     wificond \
