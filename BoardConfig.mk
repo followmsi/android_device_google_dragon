d65fd326d43b6632b74bdda471043fa9fe619b13
Yifan Hong <elsk@google.com>
2017-05-09 15:38:07 -0700

    Add device compatibility matrix
    Test: m system_manifest.xml -j
    Bug: 37954458
    Change-Id: Iedcbba5d59e407d1d84d60a58f3a79843066c68d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -123,3 +123,4 @@ BOARD_PERFSETUP_SCRIPT := platform_testing/scripts/perf-setup/dragon-setup.sh
 
 # Vendor Interface Manifest
 DEVICE_MANIFEST_FILE := device/google/dragon/manifest.xml
+DEVICE_MATRIX_FILE := device/google/dragon/compatibility_matrix.xml
