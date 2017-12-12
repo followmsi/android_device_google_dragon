0975d71ff0cf8a48957f90db2f806784b5ead930
Adrian DC <radian.dc@gmail.com>
2017-12-12 22:11:37 +0100

    Enable Treble OMX by default
    * Use the Oreo Treble mediacodec service instead
    
    Change-Id: Ia54b742693468a58abc188f78c9173ae2a66ad07

--- device.mk
+++ device.mk
@@ -213,10 +213,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.audio.monitorRotation=true \
     ro.frp.pst=/dev/block/platform/700b0600.sdhci/by-name/PST
 
-# Default OMX service to non-Treble
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.media.treble_omx=false
-
 # ro.product.first_api_level indicates the first api level the device has commercially launched on.
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.first_api_level=23
