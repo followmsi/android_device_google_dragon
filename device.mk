64226f7930c1f2b8ede9197d56466fcb580d3883
followmsi <followmsi@gmail.com>
2018-06-07 20:44:25 +0200

    dragon: fix PRODUCT_PROPERTY_OVERRIDES

--- device.mk
+++ device.mk
@@ -156,6 +156,23 @@ PRODUCT_PACKAGES += \
     lights.dragon \
     sensors.dragon
 
+#TODO(dgreid) is this right?
+PRODUCT_PROPERTY_OVERRIDES := \
+    wifi.interface=wlan0 \
+    ro.hwui.texture_cache_size=86 \
+    ro.hwui.layer_cache_size=56 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.path_cache_size=40 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=1024 \
+    ro.hwui.disable_scissor_opt=true \
+    ro.recents.grid=true
+
 # mobile data provision prop
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.prov_mobiledata=false
@@ -175,23 +192,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true \
     ro.com.widevine.cachesize=16777216
-
-#TODO(dgreid) is this right?
-PRODUCT_PROPERTY_OVERRIDES := \
-    wifi.interface=wlan0 \
-    ro.hwui.texture_cache_size=86 \
-    ro.hwui.layer_cache_size=56 \
-    ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.path_cache_size=40 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=6 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=1024 \
-    ro.hwui.disable_scissor_opt=true \
-    ro.recents.grid=true
 
 # The default locale should be determined from VPD, not from build.prop.
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
