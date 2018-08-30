5f17b6f284bbc2b1ccbeccf30d5ee3095306e66f
followmsi <followmsi@gmail.com>
2018-08-30 17:01:55 +0200

    dragon: set build fingerprint instead of override

--- aosp_dragon.mk
+++ aosp_dragon.mk
@@ -38,6 +38,6 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=dragon \
-    BUILD_FINGERPRINT=google/ryu/dragon:8.1.0/OPM4.171019.021.Y1/4903051:user/release-keys \
     PRIVATE_BUILD_DESC="ryu-user OPM4.171019.021.Y1 4903051 release-keys"
 
+BUILD_FINGERPRINT := google/ryu/dragon:8.1.0/OPM4.171019.021.Y1/4903051:user/release-keys
