579ac7a3b2b5606bca34923c0f5b8011182924f3
followmsi <followmsi@gmail.com>
2018-09-06 15:03:56 +0200

    dragon: adjust TARGET_KERNEL_TOOLCHAIN_ROOT

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,7 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+BUILD_TOP := $(shell pwd)
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 #BOARD_KERNEL_CMDLINE :=
@@ -39,7 +40,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 TARGET_KERNEL_SOURCE := kernel/google/tegra
 TARGET_KERNEL_CONFIG := lineageos_dragon_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-TARGET_KERNEL_TOOLCHAIN_ROOT := prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
+TARGET_KERNEL_TOOLCHAIN_ROOT := $(BUILD_TOP)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
 TARGET_KERNEL_TOOLS_PREFIX := $(TARGET_KERNEL_TOOLCHAIN_ROOT)/bin/aarch64-linux-android-
 BOARD_KERNEL_IMAGE_NAME := Image.fit
 
