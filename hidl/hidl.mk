ee0e562b410683021e8ebdefbb00e04136f0985d
followmsi <followmsi@gmail.com>
2018-09-08 20:37:55 +0200

    dragon: Gatekeeper/Keymaster: build 32-Bit too

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -32,7 +32,7 @@ PRODUCT_PACKAGES += \
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-impl:64 \
+    android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
 # Health
@@ -42,7 +42,7 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl:64 \
+    android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
 # Lights
