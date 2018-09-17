3caae3203a602fe307aa7921ae1baec95e96bf45
vartom <babanakov_dima@mail.ru>
2018-09-17 22:47:13 +0700

    Fix aosp build.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2015 The Android Open-Source Project
+# Copyright 2015 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,10 +14,5 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/aosp_dragon.mk \
-	$(LOCAL_DIR)/aosp_dragon_car.mk
-
-#COMMON_LUNCH_CHOICES := \
-#	aosp_dragon-userdebug \
-#	aosp_dragon-eng
+add_lunch_combo aosp_dragon-userdebug
+add_lunch_combo aosp_dragon-eng
