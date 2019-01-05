6d654628be94f738caec2e483f794d39bec13cc5
Hamster Tian <haotia@gmail.com>
2019-01-05 23:58:15 +0800

    dragon: allow overriding WiFi country code with a property
    Use persist.sys.wifi.country_code to override WiFi country code.
    This is needed if the device is not being used where it was sold.

--- init_regions.sh
+++ init_regions.sh
@@ -25,4 +25,11 @@ case "${REGION}" in
 esac
 
 setprop ro.product.locale "${LANGUAGE}-${COUNTRY}"
+
+OVERRIDE_CC=$(getprop persist.sys.wifi.country_code)
+
+if [ -n "$OVERRIDE_CC" ]; then
+  COUNTRY=$OVERRIDE_CC
+fi
+
 setprop ro.boot.wificountrycode "${COUNTRY}"
