251c0fe9234c260a6262f37f936675f8554ff4bc
followmsi <followmsi@gmail.com>
2019-04-02 08:59:16 +0200

    BUILD_FINGERPRINT - OPM8.190405.001 - android-8.1.0_r63 - 04-2019

--- aosp_dragon.mk
+++ aosp_dragon.mk
@@ -38,6 +38,6 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=dragon \
-    PRIVATE_BUILD_DESC="ryu-user 8.1.0 OPM8.190305.001 5238107 release-keys"
+    PRIVATE_BUILD_DESC="ryu-user 8.1.0 OPM8.190405.001 5321448 release-keys"
 
-BUILD_FINGERPRINT := google/ryu/dragon:8.1.0/OPM8.190305.001/5238107:user/release-keys
+BUILD_FINGERPRINT := google/ryu/dragon:8.1.0/OPM8.190405.001/5321448:user/release-keys
