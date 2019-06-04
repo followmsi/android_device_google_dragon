dabe9eece7ec3804a158aba0efbc43a1dca88873
followmsi <followmsi@gmail.com>
2019-06-04 11:24:44 +0200

    BUILD_FINGERPRINT - OPM8.190605.003 - android-8.1.0_r65 - 06-2019
    Change-Id: I7e653a10529177f937027a3cafba14fcf5928831

--- lineage.mk
+++ lineage.mk
@@ -31,9 +31,9 @@ PRODUCT_MANUFACTURER := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=dragon \
-    PRIVATE_BUILD_DESC="ryu-user 8.1.0 OPM8.190505.001 5373745 release-keys"
+    PRIVATE_BUILD_DESC="ryu-user 8.1.0 OPM8.190605.003 5524042 release-keys"
 
-BUILD_FINGERPRINT := google/ryu/dragon:8.1.0/OPM8.190505.001/5373745:user/release-keys
+BUILD_FINGERPRINT := google/ryu/dragon:8.1.0/OPM8.190605.003/5524042:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.recents.grid=true
