9f8f61189fbb633b3fdada6f13ef5f458d4619cd
followmsi <followmsi@gmail.com>
2019-08-05 10:42:59 +0200

    dragon: switch back fingerprint to pass SafetyNet
    - SafetyNet requires older fingerprint which is not linked to security patch level.
    - OPM1.171019.016 - android-8.1.0_r10 - 02-2018

--- aosp_dragon.mk
+++ aosp_dragon.mk
@@ -53,6 +53,6 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=dragon \
-    PRIVATE_BUILD_DESC="ryu-user 8.1.0 OPM8.190605.003 5524042 release-keys"
+    PRIVATE_BUILD_DESC="ryu-user 8.1.0 OPM1.171019.016 4503492 release-keys"
 
-BUILD_FINGERPRINT := google/ryu/dragon:8.1.0/OPM8.190605.003/5524042:user/release-keys
+BUILD_FINGERPRINT := google/ryu/dragon:8.1.0/OPM1.171019.016/4503492:user/release-keys
