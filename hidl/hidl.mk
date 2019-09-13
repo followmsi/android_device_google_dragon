5b24c8b935d1ef41cb1e350bb435be5e0eab0250
Justin Yun <justinyun@google.com>
2019-09-13 16:02:44 +0000

    Build VNDK
    * First effort in getting Dragon treble-compatible
    
    Change-Id: Id74a771c80307e7d2d16aca76eb9329779e049e3

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -65,6 +65,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# VNDK
+PRODUCT_PACKAGES += \
+    vndk-sp
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
