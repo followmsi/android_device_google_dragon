86cb9081354e9142a5eabc195a93b0b8229e116f
vartom <babanakov_dima@mail.ru>
2019-10-11 20:31:28 +0700

    dragon: Remove bluetooth.audio

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -14,7 +14,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.2-impl \
-    android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.audio@2.0-service
 
 # Bluetooth
