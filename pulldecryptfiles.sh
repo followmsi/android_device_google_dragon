0bbbe8e000f3d2db9512c7ec415c3e679b049132
followmsi <followmsi@gmail.com>
2019-11-02 13:08:00 +0100

    dragon: twrp: Update pulldecryptfiles.sh
     - Add workaround for system-as-root (SAR) / 2 stage init (2SI)
     - Don't copy /system/bin nor /vendor/bin folders anymore.
     - Drop quota checks before mounting /data partition
     - Clean up
    
    Change-Id: If4e655586994b37e34a7308cde6e5f54753a9e56

--- pulldecryptfiles.sh
+++ pulldecryptfiles.sh
@@ -4,41 +4,28 @@
 # allowing us to leave /vendor and /system unmounted
 
 if [ "$(mount|grep "/dev/block/bootdevice/by-name/APP on /system ")" == "" ];then
-        mount -o rw,remount rootfs /
-	mkdir /system
-        mount -t ext4 -o ro /dev/block/bootdevice/by-name/APP /system
-	mkdir /tmp/system
+	mount -o rw,remount rootfs /
+	mkdir -p /system
+	mkdir -p /tmp/system
+	mount -t ext4 -o ro /dev/block/bootdevice/by-name/APP /system
 	cp -r /system/lib64 /tmp/system/
-	cp -r /system/bin /tmp/system/
+	cp -r /system/system/lib64 /tmp/system/
 	umount /system
-	mkdir /system/lib64
-	mkdir /system/bin
-	mv /tmp/system/lib64/* /system/lib64/
-	mv /tmp/system/bin/* /system/bin/
-	rmdir /tmp/system/lib64 /tmp/system/bin /tmp/system
+	mv /tmp/system/* /system/
+	rmdir /tmp/system
 fi
 
 if [ "$(mount|grep "/dev/block/bootdevice/by-name/VNR on /vendor")" == "" ];then
 	mkdir -p /vendor
 	mkdir -p /tmp/vendor
-        mount -t ext4 -o ro /dev/block/bootdevice/by-name/VNR /vendor
-
+	mount -t ext4 -o ro /dev/block/bootdevice/by-name/VNR /vendor
 	cp -r /vendor/lib64 /tmp/vendor/
-	cp -r /vendor/bin /tmp/vendor/
-
 	umount /vendor
-
 	mv /tmp/vendor/* /vendor/
 	rmdir /tmp/vendor
 fi
 
-if [ "$(mount|grep "/dev/block/bootdevice/by-name/UDA on /data")" == "" ];then
-	LD_LIBRARY_PATH=/system/lib64 /system/bin/e2fsck  -y -E journal_only /dev/block/bootdevice/by-name/userdata
-        LD_LIBRARY_PATH=/system/lib64 /system/bin/tune2fs -Q ^usrquota,^grpquota,^prjquota /dev/block/bootdevice/by-name/userdata
-        mount /dev/block/bootdevice/by-name/UDA /data
-fi
-
-
 setprop pulldecryptfiles.finished 1
+
 exit 0
 
