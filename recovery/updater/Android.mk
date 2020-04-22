d8543f30022b15e0ad51a76b545662808743e277
vartom <babanakov_dima@mail.ru>
2020-04-23 01:07:33 +0700

    Dragon: Fix build fwtool

--- recovery/updater/Android.mk
+++ recovery/updater/Android.mk
@@ -33,12 +33,13 @@ LOCAL_SRC_FILES := \
     vboot_interface.c \
     recovery_ui.cpp
 # For ui.h, common.h (included by recovery_ui.cpp).
-LOCAL_C_INCLUDES += bootable/recovery
+LOCAL_C_INCLUDES := \
+    bootable/recovery \
+    bootable/recovery/edify/include \
+    bootable/recovery/otautil/include
 # For vboot_struct.h
 LOCAL_C_INCLUDES += external/vboot_reference/firmware/include
 LOCAL_CFLAGS += -Werror
-LOCAL_STATIC_LIBRARIES := \
-    libedify
 
 include $(BUILD_STATIC_LIBRARY)
 
@@ -50,9 +51,13 @@ LOCAL_SRC_FILES := flash_ec.c flash_mtd.c flash_device.c vboot_interface.c debug
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_CFLAGS += -Wno-unused-parameter -DUSE_LOGCAT
 # For vboot_struct.h
+LOCAL_C_INCLUDES := \
+    bootable/recovery \
+    bootable/recovery/edify/include \
+    bootable/recovery/otautil/include
 LOCAL_C_INCLUDES += external/vboot_reference/firmware/include
-LOCAL_STATIC_LIBRARIES := \
-    libedify
+
+LOCAL_HEADER_LIBRARIES :=  libhardware_headers
 
 include $(BUILD_EXECUTABLE)
 
