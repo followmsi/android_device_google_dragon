20067b03e7106c108674903e37d1c381408f79ab
followmsi <followmsi@gmail.com>
2020-08-31 18:54:10 +0200

    dragon: binderize drm

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -26,7 +26,9 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.2-service.clearkey
 
 # Dumpstate
 PRODUCT_PACKAGES += \
