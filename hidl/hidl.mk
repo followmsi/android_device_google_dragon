38917df87f0c88572ffedf72a7c43f128f4c75ba
followmsi <followmsi@gmail.com>
2020-12-08 08:42:19 +0100

    dragon: Add audio@2.0-service and soundtrigger@2.2-service

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -1,8 +1,10 @@
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.soundtrigger@2.2-impl
+    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.2-service
 
 # Bluetooth
 PRODUCT_PACKAGES += \
