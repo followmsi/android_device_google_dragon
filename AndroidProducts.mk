f2cb0b6f0ec4cb9538dcc9d89c7bd406da389b34
michie <tha_michie@hotmail.com>
2020-12-10 03:25:28 -0800

    Pixel-fy

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,9 +17,7 @@
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/aosp_dragon.mk \
 	$(LOCAL_DIR)/aosp_dragon_car.mk \
-	$(LOCAL_DIR)/lineage_dragon.mk
 
 COMMON_LUNCH_CHOICES := \
 	aosp_dragon-userdebug \
-	aosp_dragon-eng \
-	lineage_dragon-userdebug
+	aosp_dragon-eng
