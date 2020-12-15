09a30d5b8b03ddf14f83936e6997769320f3bce0
followmsi <followmsi@gmail.com>
2020-12-15 15:57:24 +0100

    dragon: Allow adb root

--- device.mk
+++ device.mk
@@ -189,10 +189,13 @@ $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-he
 
 # set default USB configuration
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp,adb \
-    ro.adb.secure=0 \
     ro.sf.lcd_density=320 \
     ro.opengles.version=196610
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.sys.usb.config=adb \
+    ro.adb.secure=0 \
+    service.adb.root=1
 
 # Update the recovery image only if the option is enabled
 # under Developer options
