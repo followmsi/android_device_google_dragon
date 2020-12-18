b950c97234c5d4689bcc1462743d46df7b714881
michie <tha_michie@hotmail.com>
2020-12-18 10:52:56 +0100

    dragon: Switch to Audio HAL V6
    Keep audio.common@5.0 & audio.common@5.0-util on V5
    
    Change-Id: I391f76c2d8c3f63b7eda699b34deaa6d30bf0051

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -4,7 +4,13 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.soundtrigger@2.2-impl \
-    android.hardware.soundtrigger@2.2-service
+    android.hardware.soundtrigger@2.2-service \
+    android.hardware.audio@6.0 \
+    android.hardware.audio.common@5.0 \
+    android.hardware.audio.common@5.0-util \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0 \
+    android.hardware.audio.effect@6.0-impl
 
 # Bluetooth
 PRODUCT_PACKAGES += \
