05c23052bd0168379f38c9e23218f070ce7c072b
followmsi <followmsi@gmail.com>
2021-03-15 18:45:21 +0100

    Revert "Revert "dragon: add properties for userspace lmkd""
    This reverts commit 22f519de3d7f2863581cf08899c8c7d9840045a2.

--- device.mk
+++ device.mk
@@ -316,6 +316,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.stagefright.ccodec=0
 
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=800 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.bionic.ld.warning=0
 
 $(call inherit-product-if-exists, hardware/nvidia/tegra132/tegra132.mk)
