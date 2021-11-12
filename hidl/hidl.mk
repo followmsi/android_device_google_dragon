374958ab23dd746e10c54f63736ebacb223c9023
followmsi <followmsi@gmail.com>
2021-11-12 13:27:10 +0100

    dragon: Use keymaster@4.0

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -55,8 +55,8 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@4.0-impl \
+    android.hardware.keymaster@4.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
