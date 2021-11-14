d3bf8c06382ada0c565493f058209f28c0052039
followmsi <followmsi@gmail.com>
2021-11-14 15:39:50 +0100

    dragon: Remove the deprecated ConfigStore HAL

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -81,10 +81,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
-# Configstore
-PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.1-service
-
 # VNDK
 PRODUCT_PACKAGES += \
     vndk-sp
