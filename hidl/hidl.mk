66d78b4957796438133661af46ce3d55c29105c9
michie <tha_michie@hotmail.com>
2022-10-19 21:27:23 +0200

    dragon: Add custom audio service

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -1,7 +1,7 @@
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
-    android.hardware.audio@2.0-service \
+    android.hardware.audio.service.dragon \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     android.hardware.soundtrigger@2.2-service \
