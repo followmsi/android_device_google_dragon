dc99166175a2838aeac22dc7e6b348125b770122
michie <tha_michie@hotmail.com>
2022-11-01 11:06:14 +0100

    Dragon: Update build deps checking macros to use clang binutils

--- vendor/build/prebuilt.mk
+++ vendor/build/prebuilt.mk
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-GCC_HOST_BINARIES:=$(wildcard prebuilts/gcc/linux-x86/host/x86_64-linux-*/x86_64-linux/bin)
-LOCAL_SHARED_LIBRARIES += $(shell (basename -s .so `$(GCC_HOST_BINARIES)/objdump -x $(LOCAL_PATH)/$(LOCAL_SRC_FILES) 2>/dev/null |grep NEEDED` 2>/dev/null |grep -v ^NEEDED$ |tr '\n' ' '))
-LOCAL_SHARED_LIBRARIES += $(shell (basename -s .so `$(GCC_HOST_BINARIES)/objdump -x $(LOCAL_PATH)/$(LOCAL_SRC_FILES_32) 2>/dev/null |grep NEEDED` 2>/dev/null |grep -v ^NEEDED$ |tr '\n' ' '))
-LOCAL_SHARED_LIBRARIES += $(shell (basename -s .so `$(GCC_HOST_BINARIES)/objdump -x $(LOCAL_PATH)/$(LOCAL_SRC_FILES_64) 2>/dev/null |grep NEEDED` 2>/dev/null |grep -v ^NEEDED$ |tr '\n' ' '))
+CLANG_BINUTILS := $(abspath prebuilts/clang/host/$(HOST_PREBUILT_TAG)/llvm-binutils-stable)
+LOCAL_SHARED_LIBRARIES += $(shell (basename -s .so `$(CLANG_BINUTILS)/llvm-objdump -x $(LOCAL_PATH)/$(LOCAL_SRC_FILES) 2>/dev/null |grep NEEDED` 2>/dev/null |grep -v ^NEEDED$ |tr '\n' ' '))
+LOCAL_SHARED_LIBRARIES += $(shell (basename -s .so `$(CLANG_BINUTILS)/llvm-objdump -x $(LOCAL_PATH)/$(LOCAL_SRC_FILES_32) 2>/dev/null |grep NEEDED` 2>/dev/null |grep -v ^NEEDED$ |tr '\n' ' '))
+LOCAL_SHARED_LIBRARIES += $(shell (basename -s .so `$(CLANG_BINUTILS)/llvm-objdump -x $(LOCAL_PATH)/$(LOCAL_SRC_FILES_64) 2>/dev/null |grep NEEDED` 2>/dev/null |grep -v ^NEEDED$ |tr '\n' ' '))
 
 include $(BUILD_PREBUILT)
