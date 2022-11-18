936cfa5e385fabe79724947eab6f256a9fed8410
michie <tha_michie@hotmail.com>
2022-11-18 15:16:07 +0100

    Dragon: Drop custom cameraprovider

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -20,7 +20,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service.dragon \
+    android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     libcameraservice
