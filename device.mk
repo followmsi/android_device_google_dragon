fd250bf90a1980123d0445927776b2862c666663
michie <tha_michie@hotmail.com>
2023-02-01 15:02:23 +0100

    Dragon: configs: Add display config file
    * DisplayPowerController[0]: Screen brightness nits configuration is unavailable; falling back
    
    * DisplayDeviceConfig: requesting nits when no mapping exists.
    
    Change-Id: I4946670e9b2c9e9ee443e62c2f91296abedcdb39

--- device.mk
+++ device.mk
@@ -86,6 +86,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
 
+# Display Device Config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
+
 # Dragon
 PRODUCT_PACKAGES += \
     DragonParts \
