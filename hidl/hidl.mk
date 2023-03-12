6a7afcc6044d7db40b5cca59904bfed3c335cc29
michie <tha_michie@hotmail.com>
2023-03-12 10:31:14 +0100

    Dragon: Drop DRM HIDL

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -28,8 +28,6 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
     android.hardware.drm-service.clearkey
 
 # Gatekeeper
