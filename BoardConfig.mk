4f07d1e70b3e3c6493926077c72c8d8ea2a9537e
michie <tha_michie@hotmail.com>
2023-04-23 15:33:28 +0200

    Dragon: Fix typo
    Change-Id: Ie2574bac5826731d29f50de0d276518b2d9433e4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,7 +85,7 @@ BOARD_USES_DRM_HWCOMPOSER                      := true
 BOARD_DRM_HWCOMPOSER_BUFFER_IMPORTER           := nvidia-gralloc
 BOARD_USES_LIBDRM                              := true
 TARGET_RECOVERY_PIXEL_FORMAT                   := BGRA_8888
-TARGET_USES_HWC1 := true                       := true
+TARGET_USES_HWC1                               := true
 
 # Permissions
 TARGET_FS_CONFIG_GEN += device/google/dragon/config.fs
